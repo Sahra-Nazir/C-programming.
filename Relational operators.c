e5a60184052269732c08fd539caa2bffcbd4fe01
sk-saru <sahrakirmani@gmail.com>
2025-05-11 17:34:44 +0530

    Relational operator

---
+++ Relational operators.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+  int a = 10;
+  int b = 20;
+
+  // Equal to (==)
+  printf("a == b: %d\n", a == b);
+
+  // Not equal to (!=)
+  printf("a != b: %d\n", a != b);
+
+  // Greater than (>)
+  printf("a > b: %d\n", a > b);
+
+  // Less than (<)
+  printf("a < b: %d\n", a < b);
+
+  // Greater than or equal to (>=)
+  printf("a >= b: %d\n", a >= b);
+
+  // Less than or equal to (<=)
+  printf("a <= b: %d\n", a <= b);
+
+  return 0;
+}
